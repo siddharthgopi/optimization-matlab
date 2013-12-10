82f5e6dffcd710ac4c4b1ee0dd3e693b4cc9925d
siddharth gopinathan <leysg1@nottingham.ac.uk>
2013-12-10 15:00:45 +0000

    prettifying code

--- optim.m
+++ optim.m
@@ -7,7 +7,6 @@ delta = 0.05;
 theta = 0.34;
 A= 1 ;
 
-
 %calculate steady state
 kstar=((1/beta-(1-delta))/(A*theta))^(1/(theta-1));
 
@@ -18,11 +17,8 @@ klo=kstar*0.9;
 khi=kstar*1.1; 
 
 step =(khi-klo)/N;
-
 k = klo:step:khi;
-
 n=length(k)
-
 
 %creating a matrix for V0 with proper dimensions
 %first guess assumes all k is immediately consumed
@@ -31,12 +27,10 @@ ktheta =k.^theta;
 colones = ones(n,1);  %create a column vector of ones
 s = colones*ktheta;   %create a matrix of kthetas 
 s1= colones*k;        %create another matrix of k's
+
 ytot = s'+(1-delta)*s1';  %this is matrix of -- ktheta + (1-d)k -- minus kt+1 but it is 0
 
 v =(ytot.^(1-gamma)-1)/(1-gamma) %applying the u function to this matrix
-
-
-
 
 %create a part of the matrix that does not depend on Bw(kt+1)
 rowones = colones';
@@ -44,4 +38,9 @@ I = k'*rowones
 J = colones*k;
 C = (J.^theta) + (1-delta)*J - I;
 
-U = (C.^(1-gamma)-1)/(1-gamma)  % u levels for each consumption level
+U = (C.^(1-gamma)-1)/(1-gamma)  % u levels for each c level
+
+%next step
+r = U+beta*v;
+v1 = max(r);
+

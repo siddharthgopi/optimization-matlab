fa8dbc82827f5977c0fe2cf708b2a47ba287e780
siddharth gopinathan <leysg1@nottingham.ac.uk>
2013-12-10 14:48:33 +0000

    /Users/siddharth/Desktop/github/optim.m

--- optim.m
+++ optim.m
@@ -8,7 +8,7 @@ theta = 0.34;
 A= 1 ;
 
 
-% steady state oh shii
+%calculate steady state
 kstar=((1/beta-(1-delta))/(A*theta))^(1/(theta-1));
 
 %intialize k-grid
@@ -31,11 +31,17 @@ ktheta =k.^theta;
 colones = ones(n,1);  %create a column vector of ones
 s = colones*ktheta;   %create a matrix of kthetas 
 s1= colones*k;        %create another matrix of k's
-ytot = s'+(1-delta)*s1';  %this is matrix of -- ktheta + (1-d)k -- minus kt+1 but it is 0 so fuck it
+ytot = s'+(1-delta)*s1';  %this is matrix of -- ktheta + (1-d)k -- minus kt+1 but it is 0
 
-v =(ytot.^(1-gamma)-1)/(1-gamma) %applying u(c) to this matrix
-
-
+v =(ytot.^(1-gamma)-1)/(1-gamma) %applying the u function to this matrix
 
 
 
+
+%create a part of the matrix that does not depend on Bw(kt+1)
+rowones = colones';
+I = k'*rowones
+J = colones*k;
+C = (J.^theta) + (1-delta)*J - I;
+
+U = (C.^(1-gamma)-1)/(1-gamma)  % u levels for each consumption level

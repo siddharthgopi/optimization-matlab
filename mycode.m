c5e2a190c91ee9281e9e34afb1823fc44a70c049
siddharth gopinathan <leysg1@nottingham.ac.uk>
2013-12-13 01:47:26 +0000

    create v0 matrix so far

---
+++ mycode.m
@@ -0,0 +1,41 @@
+clear;
+
+% parameters
+beta = 0.96 ;
+gamma = 2 ;
+delta = 0.05;
+theta = 0.34;
+A= 1 ;
+
+
+%calculate steady state
+kstar=((1/beta-(1-delta))/(A*theta))^(1/(theta-1));
+
+%intialize k-grid
+N=100;
+
+klo=kstar*0.9;
+khi=kstar*1.1; 
+
+step =(khi-klo)/N;
+
+k = klo:step:khi;
+
+n=length(k)
+
+
+%creating a matrix for V0 with proper dimensions
+%first guess assumes all k is immediately consumed
+
+ktheta =k.^theta;
+colones = ones(n,1);  %create a column vector of ones
+s = colones*ktheta;   %create a matrix of kthetas 
+s1= colones*k;        %create another matrix of k's
+ytot = s'+(1-delta)*s1';  %this is matrix of -- ktheta + (1-d)k -- minus kt+1 but it is 0
+
+v =(ytot.^(1-gamma)-1)/(1-gamma) %applying the utility function to this matrix
+
+
+
+
+

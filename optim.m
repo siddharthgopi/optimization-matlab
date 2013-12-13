c440fd6fd1975faa4f3bc1af6c28b018bdcf59cc
siddharth gopinathan <leysg1@nottingham.ac.uk>
2013-12-13 12:30:46 +0000

    change some stuff

--- optim.m
+++ optim.m
@@ -1,15 +1,13 @@
 clear;
 
-
-gridsize = input('Please specify a size for the k-grid; ')
+gridsize = input('Dr.Alex, please specify a size for the k-grid; ')
 eps = input('Please specify your precision parameter; ')
 
-
 % parameters
-beta = 0.96 ;
-gamma = 2 ;
-delta = 0.05;
-theta = 0.34;
+beta = 0.95 ;
+gamma = 3.2938;
+delta = 0.1;
+theta = 0.33;
 A= 1 ;
 
 %calculate steady state
@@ -28,7 +26,6 @@ n=length(k);
 
 %creating a matrix for V0 with proper dimensions
 %first guess assumes all k is immediately consumed
-
 ktheta =k.^theta;
 colones = ones(n,1);  %create a column vector of ones
 s = colones*ktheta;   %create a matrix of kthetas 
@@ -44,31 +41,35 @@ v =(ytot.^(1-gamma)-1)/(1-gamma); %applying the u function to this matrix
 rowones = colones';
 I = k'*rowones;
 J = colones*k;
-C = (J.^theta) + (1-delta)*J - I;
-C2= (k'.^theta) + (1-delta)*k';
+C = (J.^theta) + (1-delta)*J - I;  %nxn matrix
 
-U = (C.^(1-gamma)-1)/(1-gamma);  % u levels for each c level
+C2= (k'.^theta) + (1-delta)*k';    %this particular vector was created to
+                                   %help calculate vector representing
+                                   %stable arm for later use.
+                                   
+U = (C.^(1-gamma)-1)/(1-gamma);  % util levels for each consumption level
 
-%rule for choosing next iter
-r = U+beta*v;
-v1 = max(r);
 
-%actual iterations
+r = U+beta*v;    % adding the time variant part back
+v1 = max(r);     % Row vector of only the maximum of each column of r
+
+%The main loop
 change = eps;
 
-while change >= eps
-    v1old = v1;
-    w= ones(n,1)*v1;
-    w1 = U+beta*w';
-    v1 = max(w1);
-    change = norm(v1-v1old,2);
+while change >= eps             % while distance is above a threshold
+    v1old = v1;                 % assigning v1old to calculate euc.distance later
+    w= ones(n,1)*v1;            % since v1 is a row vector, we make it a matrix
+    w1 = U+beta*w';             % adding value of carrying forward kt+1_i level of capital
+    v1 = max(w1);               % getting back the v1 vector for next iteration
+    change = norm(v1-v1old,2);  % Finds euclidean distance of vectors
     
-    change;
+    change;                     %i turn this on when im playing around with 
+                                % parameter values to see the convergence
     
-   
 end
 
 [val,ind] = max(w1); %save the values and corresponding index
+
 optk = k(ind);       %this represents the optimal capital accumulation policy
 
 optc = C2-optk';     %optimal consumpion values, applied the formula from the 
@@ -76,27 +77,51 @@ optc = C2-optk';     %optimal consumpion values, applied the formula from the
                      %dimensions
                      
 
-deltak0 = A*k.^theta-delta*k; %represents curve ct = f(kt) - dkt
-deltac0=deltak0+k-kstar;      %similarly this represents points where ...
+deltak0 = A*k.^theta-delta*k; %represents curve ct = f(kt) - dkt, i.e. change in k is 0
+
+deltac0=deltak0+k-kstar;      %this represents points where,
                               %change in consumption is 0
 
                               
-%plot k against optimal k'
+
+%**** Plotting g(k), Stable Arm *****%
 
 wish = input('Press ''y'' to view Optimal Policy for Capital Accumulation, g(k), and the Stable Arm ','s');
+
 if wish == 'y'
     
 figure(1);    
 plot(k,optk',k,k,'linewidth',1); hold on;
+plot(kstar, kstar, 'r+');                        % put a red plus on the solution point
 xlabel('Current capital')
 ylabel('Optimal future capital')
+text(kstar+.1,kstar-.1,['k*=' num2str(kstar)]);  % text with solution offset by 0.1 to the right and up
+title('Optimal Policy function, g(k)')                                          
 
 figure(2);
 plot(k,optc,k,deltak0,k,deltac0)
+xlabel('k_t')
+ylabel('c_t')
+legend('Stable Arm','Delta k_t = 0','Delta c_t = 0') 
+title('Phase Diagram')
+
+figure(3);
+plot(k,optc)
+xlabel('k_t')
+ylabel('c_t')
+legend('Stable Arm')
+title('Stable Arm Singled out')
+
+
+figure(4);
+plot(k,v1)
+xlabel('Choice of kt+1')
+ylabel('Value function')
+title('Value function against k')
 
 
 else disp('End')
-    
+ 
 end
 
 
